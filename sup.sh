36bbe5fd6bbcaf80ae895aca7fd4364dc0fc7221
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-05-05 21:41:25 +0200

    Motion Update

--- sup.sh
+++ sup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 sudo cp motion.service /usr/lib/systemd/system/motion.service
+sudo chmod 644 usr/lib/systemd/system/motion.service
+
 sudo cp liveview.service /usr/lib/systemd/system/liveview.service
+sudo chmod 644 usr/lib/systemd/system/liveview.service
+
 sudo cp bewegung.service /usr/lib/systemd/system/bewegung.service
+sudo chmod 644 usr/lib/systemd/system/bewegung.service
 #Skripte
 sudo cp bewegung.py /etc/gphoto2/bewegung.py
+
 sudo cp tasten.py /etc/gphoto2/tasten.py
 sudo cp liveView.sh /etc/gphoto2/liveview.sh# sudo systemctl enable liveview.service
 # Aktivieren der Services für den Autostart
