c38e8071a32b9a042977f17131f678363d05a345
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-05-05 22:11:38 +0200

    Einführung des Matrix Service

--- sup.sh
+++ sup.sh
@@ -2,12 +2,10 @@
 
 sudo cp motion.service /etc/systemd/system/motion.service
 sudo chmod 644 /etc/systemd/system/motion.service
-
 sudo cp liveview.service /etc/ystemd/system/liveview.service
 sudo chmod 644 /etc/systemd/system/liveview.service
-
-sudo cp bewegung.service /etc/systemd/system/bewegung.service
-sudo chmod 644 /etc/systemd/system/bewegung.service
+sudo cp bewegung.service /etc/systemd/system/matrix.service
+sudo chmod 644 /etc/systemd/system/matrix.service
 #Skripte
 sudo cp bewegung.py /etc/gphoto2/bewegung.py
 
