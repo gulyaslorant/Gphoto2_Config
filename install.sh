9d8b8cdf9898d8a25d914e1a25cc148e15713d79
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-05-05 20:27:49 +0200

    Service Updates

--- install.sh
+++ install.sh
@@ -3,10 +3,25 @@
 # Installieren von den Notwendigen Softwaretools
 sudo apt install -y gphoto2 libgphoto2*
 
-# Einrichten der Services
+# Kopieren aller Sckripts und Services in die korrekten Ordner
+#Services
+sudo cp motion.service /usr/lib/systemd/system/motion.service
 sudo cp liveview.service /usr/lib/systemd/system/liveview.service
-# sudo systemctl enable liveview.service
+sudo cp bewegung.service /usr/lib/systemd/system/bewegung.service
+#Skripte
+sudo cp bewegung.py /etc/gphoto2/bewegung.py
+sudo cp tasten.py /etc/gphoto2/tasten.py
+sudo cp liveView.sh /etc/gphoto2/liveview.sh# sudo systemctl enable liveview.service
+# Aktivieren der Services für den Autostart
+sudo systemctl enable motion.service
+sudo systemctl enable bewegung.service
+sudo systemctl enable liveview.service
+#Restart des Daemons
 sudo systemctl daemon-reload
+# Starten der Services
+sudo systemctl start motion.service
+sudo systemctl start bewegung.service
+sudo systemctl start liveview.service
 
 echo "lvstop='systemctl stop liveview.service'" >> ../.bashrc
 echo "lvstart='systemctl start liveview.service'" >> ../.bashrc
--- sup.sh
+++ sup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Kopieren aller Sckripts und Services in die korrekten Ordner
-#Services
 sudo cp motion.service /usr/lib/systemd/system/motion.service
 sudo cp liveview.service /usr/lib/systemd/system/liveview.service
 sudo cp bewegung.service /usr/lib/systemd/system/bewegung.service
 #Skripte
 sudo cp bewegung.py /etc/gphoto2/bewegung.py
 sudo cp tasten.py /etc/gphoto2/tasten.py
-sudo cp liveView.sh /etc/gphoto2/liveview.sh
+sudo cp liveView.sh /etc/gphoto2/liveview.sh# sudo systemctl enable liveview.service
+# Aktivieren der Services für den Autostart
+sudo systemctl enable motion.service
+sudo systemctl enable bewegung.service
+sudo systemctl enable liveview.service
+#Restart des Daemons
+sudo systemctl daemon-reload
+# Starten der Services
+sudo systemctl start motion.service
+sudo systemctl start bewegung.service
+sudo systemctl start liveview.service
 
 
 
