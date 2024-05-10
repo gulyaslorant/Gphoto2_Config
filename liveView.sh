7c9d346e954ed7eec66b0552b17d49dd09ccc5d2
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-05-10 19:28:11 +0200

    Update of Skript

--- liveView.sh
+++ liveView.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gphoto2 --set-config viewfinder=1
+# gphoto2 --set-config viewfinder=1
 # gphoto2 --capture-movie --stdout | mplayer -cache 128 -
 gphoto2 --capture-movie --stdout | ffmpeg -i -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 -
 # ExecStart=/usr/bin/sudo /bin/bash -lc 'gphoto2 --stdout --capture-movie | ffmpeg -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0'
--- sup.sh
+++ sup.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
+#Neueste Version vom Master holen
+git fetch
+git pull
+
+#Kopieren der entsprechenden Dateien
 sudo cp motion.service /etc/systemd/system/motion.service
 sudo chmod 644 /etc/systemd/system/motion.service
 sudo cp liveview.service /etc/ystemd/system/liveview.service
 sudo chmod 644 /etc/systemd/system/liveview.service
-sudo cp bewegung.service /etc/systemd/system/matrix.service
+sudo cp matrix.service /etc/systemd/system/matrix.service
 sudo chmod 644 /etc/systemd/system/matrix.service
+
 #Skripte
 sudo cp bewegung.py /etc/gphoto2/bewegung.py
-
 sudo cp tasten.py /etc/gphoto2/tasten.py
-sudo cp liveView.sh /etc/gphoto2/liveview.sh# sudo systemctl enable liveview.service
+sudo cp liveView.sh /etc/gphoto2/liveview.sh
+
 # Aktivieren der Services für den Autostart
 sudo systemctl enable motion.service
 sudo systemctl enable bewegung.service
-sudo systemctl enable liveview.service
+# sudo systemctl enable liveview.service
 #Restart des Daemons
 sudo systemctl daemon-reload
 # Starten der Services
 sudo systemctl start motion.service
 sudo systemctl start bewegung.service
-sudo systemctl start liveview.service
+#sudo systemctl start liveview.service
 
 
 
