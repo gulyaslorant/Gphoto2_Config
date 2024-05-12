81e36c7b9bfd9758cc0246e6e70e088ae18d60c7
Lorant Gulyas <gulyas.lorant@gmail.com>
2024-05-12 14:18:36 +0200

    Bugfix Sup.sh, Install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt install -y gphoto2 libgphoto2*
 #Services
 sudo cp motion.service /etc/systemd/system/motion.service
 sudo chmod 644 /etc/systemd/system/motion.service
-sudo cp liveview.service /etc/ystemd/system/liveview.service
+sudo cp liveview.service /etc/systemd/system/liveview.service
 sudo chmod 644 /etc/systemd/system/liveview.service
 sudo cp bewegung.service /etc/systemd/system/matrix.service
 sudo chmod 644 /etc/systemd/system/matrix.service
@@ -17,13 +17,13 @@ sudo cp tasten.py /etc/gphoto2/tasten.py
 sudo cp liveView.sh /etc/gphoto2/liveview.sh# sudo systemctl enable liveview.service
 # Aktivieren der Services für den Autostart
 sudo systemctl enable motion.service
-sudo systemctl enable bewegung.service
+sudo systemctl enable matrix.service
 sudo systemctl enable liveview.service
 #Restart des Daemons
 sudo systemctl daemon-reload
 # Starten der Services
 sudo systemctl start motion.service
-sudo systemctl start bewegung.service
+sudo systemctl start matrix.service
 sudo systemctl start liveview.service
 
 echo "lvstop='systemctl stop liveview.service'" >> ../.bashrc
--- sup.sh
+++ sup.sh
@@ -7,7 +7,7 @@ git pull
 #Kopieren der entsprechenden Dateien
 sudo cp motion.service /etc/systemd/system/motion.service
 sudo chmod 644 /etc/systemd/system/motion.service
-sudo cp liveview.service /etc/ystemd/system/liveview.service
+sudo cp liveview.service /etc/systemd/system/liveview.service
 sudo chmod 644 /etc/systemd/system/liveview.service
 sudo cp matrix.service /etc/systemd/system/matrix.service
 sudo chmod 644 /etc/systemd/system/matrix.service
